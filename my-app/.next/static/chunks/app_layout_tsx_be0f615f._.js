35f3b7691dfa58238fab5cc42d5c5bfdcf07d218
Jaydon <jaydon.christen@tacgov.com>
2025-06-05 16:07:38 -0400

    chore: clean repository and remove node_modules

--- my-app/.next/static/chunks/app_layout_tsx_be0f615f._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__9d126714._.css",
-  "static/chunks/node_modules__pnpm_13615eaa._.js",
-  "static/chunks/_676ca4cf._.js"
-],
-    source: "dynamic"
-});
